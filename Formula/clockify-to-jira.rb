525fc24faaa84f45519224a46fdc683fc1a6697e
kruc <jacek.ciwis@gmail.com>
2020-11-29 19:48:27 +0100

    Brew formula update for clockify-to-jira version v0.5.0

--- Formula/clockify-to-jira.rb
+++ Formula/clockify-to-jira.rb
@@ -2,17 +2,16 @@
 class ClockifyToJira < Formula
   desc "Software to migrate time entries from clockify to jira."
   homepage "https://example.com/"
-  version "0.4.0"
+  version "0.5.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/kruc/clockify-to-jira/releases/download/v0.4.0/clockify-to-jira_0.4.0_Darwin_x86_64.tar.gz"
-    sha256 "f1d1d19221c28ed77fe47e246a1b67cce4fc62565392b53e742a653e5e45b723"
-  elsif OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/kruc/clockify-to-jira/releases/download/v0.4.0/clockify-to-jira_0.4.0_Linux_x86_64.tar.gz"
-      sha256 "95a77b8296812bccd8329ac914e430a03ef6882bbe92c1f118f0a21fe184968b"
-    end
+    url "https://github.com/kruc/clockify-to-jira/releases/download/v0.5.0/clockify-to-jira_0.5.0_Darwin_x86_64.tar.gz"
+    sha256 "1f6cd93fdabacb648f5ab504f3fa5ee43ff31b5a531bd1b472cc6ab30d09f177"
+  end
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/kruc/clockify-to-jira/releases/download/v0.5.0/clockify-to-jira_0.5.0_Linux_x86_64.tar.gz"
+    sha256 "8ddda17f4cb66e7cce1ab25b6789a58784b0366b3a9b02900c10d3505a68e756"
   end
 
   def install
